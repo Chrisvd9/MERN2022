3b2ab93e0536cdf7051ec198db29eaa05abf82ee
Christian Vergara <90857683+Chrisvd9@users.noreply.github.com>
2022-03-24 00:52:47 -0300

    Delete APIs/luke-apiwalker directory

--- APIs/luke-apiwalker/src/actions/simpleGet3.js
+++
@@ -1,8 +0,0 @@
-export async function simpleGet3(url){
-    try{
-        const apiResult = await fetch(url).then((response)=> response.json())
-        return apiResult;
-    }catch (error){
-        return {error: `Se ha producido un: ${error}`}
-    }
-}
