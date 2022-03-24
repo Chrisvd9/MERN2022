f06400c4029d63675bf82065bc70f0cfc780d95f
Christian Vergara <90857683+Chrisvd9@users.noreply.github.com>
2022-03-24 00:52:05 -0300

    Delete APIs/luke-apiwalker/src/services directory

--- APIs/luke-apiwalker/src/services/simpleGets.js
+++
@@ -1,8 +0,0 @@
-export async function simpleGets(url){
-    try{
-        const apiResponse = await fetch(url).then((response)=> response.json);
-        return apiResponse
-    }catch(error){
-        return {error: `Fallo en la API ${error}`}
-    }
-}
